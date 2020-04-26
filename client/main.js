715c4a34d3f4497833c95c620f56b9de036ab393
johancuda <61778212+johancuda@users.noreply.github.com>
2020-04-26 12:42:09 +0200

    Posts
    Ajout des posts à la collection

--- client/main.js
+++ client/main.js
@@ -1,5 +1,28 @@
-import '../both/collections.js';
+/* eslint-disable no-param-reassign */
+/* eslint-disable meteor/template-names */
+
 
 import './main.html';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Posts } from '../both/collections.js';
+
+// eslint-disable-next-line no-undef
+Template.form_new_post.events({
+  // eslint-disable-next-line no-unused-vars
+  'submit .js-new-post' (event, templateInstance) {
+    event.preventDefault();
+    const titleVal = event.target.title.value;
+    const textVal = event.target.text.value;
+
+    Posts.insert({
+      title: titleVal,
+      texte: textVal,
+      createdAt: new Date(),
+    });
+
+
+    event.target.title.value = '';
+    event.target.text.value = '';
+  },
+});
