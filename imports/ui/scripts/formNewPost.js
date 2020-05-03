1ab7d05d03504788d69d3ce6359e31a752c44595
johancuda <61778212+johancuda@users.noreply.github.com>
2020-05-03 20:05:55 +0200

    sweetalert pour post

--- imports/ui/scripts/formNewPost.js
+++ imports/ui/scripts/formNewPost.js
@@ -1,4 +1,7 @@
 /* eslint-disable no-param-reassign */
+/* eslint-env jquery */
+import Swal from 'sweetalert2';
+
 import { Template } from 'meteor/templating';
 
 import { Meteor } from 'meteor/meteor';
@@ -6,6 +9,9 @@ import { Meteor } from 'meteor/meteor';
 import { Posts } from '../../api/collections.js';
 
 import '../templates/formNewPost.html';
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 Template.formNewPost.events({
   // eslint-disable-next-line no-unused-vars
@@ -27,5 +33,29 @@ Template.formNewPost.events({
 
     event.target.title.value = '';
     event.target.text.value = '';
+  },
+});
+
+Template.formNewPost.events({
+  'click #bouton'(event) {
+    event.preventDefault();
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, post it!',
+    }).then((result) => {
+      if (result.value) {
+        $('form#form').submit();
+        Swal.fire(
+          'Posted!',
+          'Your file has been posted.',
+          'success',
+        );
+      }
+    });
   },
 });
