cf36f49f0180aeecde0a7727db3c30d8c95f77ce
Ikadel <isa@isatis.ch>
2020-05-06 11:56:05 +0200

    Test et base routing

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -2,6 +2,8 @@ import { Template } from 'meteor/templating';
 
 import { Meteor } from 'meteor/meteor';
 
+import { FlowRouter } from 'meteor/kadira:flow-router';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './templates/accueil.html';
@@ -11,11 +13,23 @@ import './templates/formNewPost.html';
 import './templates/listPost.html';
 import './templates/singlePost.html';
 import './body.html';
-// bouton déconnexion:
+import './templates/connexion.html';
+import './templates/test.html';
+import './scripts/test.js';
+import '../../lib/routes.js';
 
+// bouton déconnexion:
 Template.accueil.events({
   // eslint-disable-next-line no-unused-vars
   'click .js-logout'(event, templateInstance) {
     Meteor.logout();
+  },
+});
+
+// test de routing
+Template.appBody.events({
+  'click #test'(event) {
+    event.preventDefault();
+    FlowRouter.go('test');
   },
 });
