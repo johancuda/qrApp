72998f07ac3a640a880dc2517b2cea1d83500003
Ikadel <isa@isatis.ch>
2020-05-26 22:16:03 +0200

    ajout condition termes

--- imports/ui/scripts/newAccount.js
+++ imports/ui/scripts/newAccount.js
@@ -13,23 +13,28 @@ Template.newAccount.events({
     const mail = document.getElementById('mail').value;
     const mdp = document.getElementById('mdp').value;
     const mdpConf = document.getElementById('mdpConf').value;
+    const checkTerms = document.getElementById('check-terms');
 
     if (mail.includes('@unil.ch') && mail.length > 10) {
       if (mdp.length > 4) {
         if (mdp === mdpConf) {
-          Accounts.createUser({
-            username: mail,
-            password: mdp,
-            email: mail,
-            profile: { towers: [] },
-          },
-          (error) => {
-            if (error) {
-              alert(error.message);
-            } else {
-              setTimeout(() => FlowRouter.go('connexion', { id: url }), 200);
-            }
-          });
+          if (checkTerms.checked === true) {
+            Accounts.createUser({
+              username: mail,
+              password: mdp,
+              email: mail,
+              profile: { towers: [] },
+            },
+            (error) => {
+              if (error) {
+                alert(error.message);
+              } else {
+                setTimeout(() => FlowRouter.go('connexion', { id: url }), 200);
+              }
+            });
+          } else {
+            alert('Vous devez accepter les conditions d\'utilisation.');
+          }
         } else {
           alert('Vérifiez votre mot de passe.');
         }
